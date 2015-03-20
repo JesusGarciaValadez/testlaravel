9625838e40f1a3920c7d4d587a636cef9296234b
Jesus Antonio Garcia Valadez <zerovamp@hotmail.com>
2015-03-20 06:42:51 +0000

    Migrations of users, tags & user profiles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,15 +12,19 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create( 'users', function ( Blueprint $table )
 		{
-			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
+			$table->increments( 'id' );
+			$table->string( 'first_name' );
+            $table->string( 'last_name' );
+            $table->string( 'full_name' );
+			$table->string( 'email' )->unique();
+			$table->string( 'password', 60);
+            $table->boolean( 'active' )->default( true );
+            $table->enum( 'type', [ 'admin', 'user' ] );
 			$table->rememberToken();
 			$table->timestamps();
-		});
+		} );
 	}
 
 	/**
@@ -30,7 +34,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop( 'users' );
 	}
 
 }
